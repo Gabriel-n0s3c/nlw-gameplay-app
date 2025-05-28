5eab6869d9aa6e6d74b34911868eea42b5f6f735
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2025-05-28 19:29:49 -0300

    feature: cria lista de categorias e aceita icones svg

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { styles } from "./style";
+import { ScrollView } from "react-native-gesture-handler";
+import { categories } from "../../utils/categories";
+import { Category } from "../Category";
+
+type Props = {
+  categorySelected: string,
+  setCategory:(categoryId: string) => void
+};
+export function CategorySelect({ categorySelected, setCategory }: Props) {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingRight: 40,
+      }}
+      style={styles.container}
+    >
+      {categories.map((category) => (
+        <Category
+          key={category.id}
+          icon={category.icon}
+          title={category.title}
+          checked={category.id+'' == categorySelected}
+          onPress={() => setCategory(category.id+'')}
+        />
+      ))}
+    </ScrollView>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Image } from "react-native";
 import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./style";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 
 export function Home() {
+
+    const [category, setCategory] = useState('');
+
+    function handleCategorySelect(categoryId: string){
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
 
     const url = IllustrationImg
     return (
@@ -13,6 +20,11 @@ export function Home() {
             <View style={styles.header}>
                 <Profile></Profile>
                 <ButtonAdd/>
+            </View>
+            <View >
+                <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect} />
             </View>
         </View>
     );
